70cf919aadbab715b9500b7f3908e68e3b66087d
bmohitsrivastava07 <103405843+bmohitsrivastava07@users.noreply.github.com>
2023-05-28 16:39:13 +0530

    Update Review.java

--- src/main/java/com/ArtGalleryManagement/Backend/Entity/Review.java
+++ src/main/java/com/ArtGalleryManagement/Backend/Entity/Review.java
@@ -25,6 +25,10 @@ public class Review {
     @Column(name = "rating")
     private double rating;
 
+@ManyToOne(fetch = FetchType.LAZY)
+@JoinColumn(name = "product_id", referencedColumnName = "product_id", insertable = false, updatable = false)
+private Product product;
+
     @Column(name = "product_id")
     private Long productId;
 
